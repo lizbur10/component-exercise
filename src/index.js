223d72e4c38b776f22cbf336056012ddc1ac643e
lizbur10 <liz.burton147@gmail.com>
2019-03-21 16:15:08 -0400

    finished exercise

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
 
-            <CommentDetail />
-
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Sam" 
+                    date="Today at 3:00PM" 
+                    content="You're my hero!" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Alex" 
+                    date="Yesterday at 9:00AM" 
+                    content="Nice blog post" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Jessie" 
+                    date="Tuesday at 8:00PM" 
+                    content="Howdy!" 
+                />
+            </ApprovalCard>
         </div>
     );
 }
-ReactDOM.render(<App />, document.querySelector('#root'));
 
+ReactDOM.render(<App />, document.querySelector('#root'));
